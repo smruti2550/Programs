8dbf470e225ad909f5030ac225145b40329f92ae
smruti.sahoo <smruti.sahoo@pplivo.com>
2023-07-19 19:17:57 +0530

    code commit

---
+++ Script/src/ReverseWordsOfSentence.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ReverseWordsOfSentence {
+
+    public static void main(String[] argument){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter the input");
+        System.out.println(reverseSentence(scn.nextLine()));
+
+    }
+
+    static String reverseSentence(String input){
+        String[] arr= input.split(" ");
+        StringBuilder sb= new StringBuilder();
+        for (int i=arr.length-1;i>=0;i--){
+            sb.append(reverseWord(arr[i]));
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    static String reverseWord(String word){
+        String op="";
+        for (int i=word.length()-1;i>=0;i--){
+            op=op+word.charAt(i);
+        }
+        return op;
+    }
+
+}
