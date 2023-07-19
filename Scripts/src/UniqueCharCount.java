2f81ca9f5afb927a0484a0d98dd241a5ea62fa01
smruti.sahoo <smruti.sahoo@pplivo.com>
2023-07-19 20:43:22 +0530

    unqiue char

---
+++ Scripts/src/UniqueCharCount.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class UniqueCharCount {
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter the input");
+        unique(scn.nextLine());
+    }
+
+    static void unique(String input){
+        String s="";
+        char c;
+        for (int i=0;i<input.length();i++){
+            if(s.indexOf(input.charAt(i))==-1){
+                c= input.charAt(i);
+                System.out.println(c+"="+count(input,c));
+                s=s+c;
+            }
+        }
+    }
+    static int count(String input, char c){
+        return input.length()-input.replace(c+"","").length();
+    }
+}
