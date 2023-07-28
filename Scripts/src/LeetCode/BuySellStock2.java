20113dec9071973f287d370d627bcadc7d9d0e3b
smruti.sahoo <smruti.sahoo@pplivo.com>
2023-07-28 08:59:04 +0530

    multiple buy and sell

--- Scripts/src/LeetCode/BuySellStock2.java
+++ Scripts/src/LeetCode/BuySellStock2.java
@@ -7,8 +7,13 @@ public class BuySellStock2 {
 //
 //        local min-left and write is big--buy
 //        local mac- left and write is small--sell
+        int profit=0;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i+1]>arr[i]){
+                profit=profit+(arr[i+1]-arr[i]);
 
-
-
+            }
+        }
+        System.out.println(profit);
     }
 }
