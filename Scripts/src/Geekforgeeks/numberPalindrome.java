ed79f1ee92c11dc09ae37e16c50c1193f8a15bd4
smruti.sahoo <smruti.sahoo@pplivo.com>
2024-01-20 01:31:00 +0530

    factorial

--- Scripts/src/Geekforgeeks/numberPalindrome.java
+++ Scripts/src/Geekforgeeks/numberPalindrome.java
@@ -1,2 +1,18 @@
-package Geekforgeeks;public class numberPalindrome {
+package Geekforgeeks;
+
+public class numberPalindrome {
+    public static void main (String[] args){
+        System.out.println(isPalindrome(121));
+    }
+
+    static boolean isPalindrome(int n){
+        int reverseNumber=0;
+        int temp =n;
+        while (n>0){
+            int rem=n %10;
+            reverseNumber= reverseNumber*10+rem;
+            n=n/10;
+        }
+        return (reverseNumber==temp);
+    }
 }
