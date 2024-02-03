00afdb65a8d66cbd54d662041f048579b02e1968
smruti.sahoo <smruti.sahoo@pplivo.com>
2024-02-04 02:01:19 +0530

    array programs

---
+++ Scripts/src/Geekforgeeks/Arrays/RotateByN.java
@@ -0,0 +1,45 @@
+package Geekforgeeks.Arrays;
+
+import java.util.Arrays;
+
+public class RotateByN {
+    public static void main(String [] args){
+        int d=3;
+        int[] arr= {1,2,3,4,5,6,7};
+        rotate(arr,d);
+//        int[] temp= new int[d];
+//        for (int i=0;i<d;i++){
+//            temp[i]=arr[i];
+//        }
+//        for (int i=d;i<arr.length;i++){
+//            arr[i-d]= arr[i];
+//        }
+//        int j=0;
+//        for (int i=arr.length-d;i<arr.length;i++){
+//            arr[i]=temp[j];
+//            j++;
+//        }
+//        for (int k:arr){
+//            System.out.println(k);
+//        }
+
+    }
+    static void rotate(int [] arr, int d){
+        reverse(arr,0,d-1);
+        reverse(arr,d,arr.length-1);
+        reverse(arr,0,arr.length-1);
+        for (int i: arr){
+            System.out.println(i);
+        }
+
+    }
+    static void reverse(int [] arr, int start, int end){
+        while(start<end){
+            int temp=arr[end];
+            arr[end]=arr[start];
+            arr[start]=temp;
+            start++;
+            end--;
+        }
+    }
+}
---
+++ Scripts/src/Geekforgeeks/Arrays/RotateByOne.java
@@ -0,0 +1,15 @@
+package Geekforgeeks.Arrays;
+
+public class RotateByOne {
+    public static void main(String [] args){
+        int [] arr= {1,2,3,4,5};
+        int temp=arr[0];
+        for (int i=1;i<arr.length;i++){
+           arr[i-1]=arr[i];
+        }
+        arr[arr.length-1]=temp;
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+}
