a5b88b5cef7bbe347eb837a1b73596efe2d0b3c8
smruti.sahoo <smruti.sahoo@pplivo.com>
2024-02-23 01:06:48 +0530

    array programs

---
+++ Scripts/src/Geekforgeeks/Arrays/EqulibriumPoint.java
@@ -0,0 +1,19 @@
+package Geekforgeeks.Arrays;
+
+public class EqulibriumPoint {
+    public static void main (String[] args) {
+        int[] arr = {1, 5, 3, 2, 1, 4, 3};
+        for (int i=0;i<arr.length;i++){
+            int leftSum=0;
+            int rightSum=0;
+            for (int j=0;j<i;j++){
+                leftSum=leftSum+arr[j];
+            }
+            for (int k=i+1;k<arr.length;k++){
+                rightSum=rightSum+arr[k];
+            }
+            if (rightSum==leftSum) System.out.println("true");
+        }
+    }
+
+}
--- Scripts/src/Geekforgeeks/Arrays/SubarrayWithGivenSuum.java
+++ Scripts/src/Geekforgeeks/Arrays/SubarrayWithGivenSuum.java
@@ -3,16 +3,31 @@ package Geekforgeeks.Arrays;
 public class SubarrayWithGivenSuum {
     public static void main (String[] args) {
         int[] arr = {1, 5, 3, 2, 1, 4, 3};
-        System.out.println(checkSum(arr,100));
+        System.out.println(checkLongest(arr,5));
     }
     public static boolean checkSum(int [] arr, int given){
         for (int i=0;i<arr.length;i++) {
             int sum = 0;
             for (int j = i; j < arr.length; j++) {
-                sum = sum + arr[i];
+                sum = sum + arr[j];
                 if (sum == given) return true;
             }
         }
         return false;
     }
+//    lonest subarray with given sum, give the mqx index value
+    public static int checkLongest(int [] arr, int given){
+        int length=0;
+        for (int i=0;i<arr.length;i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum = sum + arr[j];
+                if (sum == given) {
+                    length=Math.max(length,(j-i+1));
+                    }
+                }
+            }
+        return length;
+    }
+
 }
